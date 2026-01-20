0cb1400ebbfe61d1daad7fc89b83e550b54cff8a
skodacreation <skcreations1011@gmail.com>
2026-01-20 11:18:46 +0530

    Add a Bug

--- ordino/e2e/ui/ordino_home.spec.ts
+++ ordino/e2e/ui/ordino_home.spec.ts
@@ -5,7 +5,7 @@ test.describe('Ordino Home Dashboard - Test Suite', () => {
     test('Verify Profile Logout', async ({ loginPage, homePage }) => {
         await loginPage.goto();
         await loginPage.step_enterUsername("admin@platform.com");
-        await loginPage.step_enterPassword("admin");
+        await loginPage.step_enterPassword("admin000");
         await loginPage.step_clickLogin();           
         await homePage.step_logout();
     });    
